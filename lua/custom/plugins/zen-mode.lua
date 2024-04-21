57ae2ba25cbb51a192f5145b4076ce59deec4698
Marius Fagerland <mariusfagerland@yahoo.no>
2024-04-21 19:46:05 +0200

    update

--- lua/custom/plugins/zen-mode.lua
+++ lua/custom/plugins/zen-mode.lua
@@ -3,8 +3,9 @@ return {
   version = '*',
   config = function()
     require('no-neck-pain').setup {
+      width = 120,
       autocmds = {
-        enableOnVimEnter = true,
+        enableOnVimEnter = false,
       },
     }
   end,
